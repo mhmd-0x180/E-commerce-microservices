7d8451c4e4b735021e13a21a8aea6f7445bf37db
mohammed <mabwsnt71@gmail.com>
2026-01-28 00:15:52 +0300

    Created Abstraction on MediatR for CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name
+        ,List<string> Catagory
+        ,string Description
+        ,string ImageFile
+        ,decimal Price):IRequest<CreateProdcutResult>;
+
+    public record CreateProdcutResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProdcutResult>
+    {
+        public Task<CreateProdcutResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
